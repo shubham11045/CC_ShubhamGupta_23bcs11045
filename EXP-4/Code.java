b235b484f2b89ad9823f874a2f58eca39423a7e6
shubham11045 <149588406+shubham11045@users.noreply.github.com>
2026-02-20 16:35:18 +0530

    Update Code.java

--- EXP-4/Code.java
+++ EXP-4/Code.java
@@ -1,1 +1,18 @@
+ public int sumBitDifferences(int[] arr) {
+        int n = arr.length;
+        long ans = 0;
 
+        for (int bit = 0; bit < 32; bit++) {
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                if ((arr[i] & (1 << bit)) != 0) {
+                    count++;
+                }
+            }
+
+            ans += (long) count * (n - count) * 2;
+        }
+
+        return (int) ans;
+    }
